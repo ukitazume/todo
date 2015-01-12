af84896d922d225548aec88417e1adb21527cc4c
Yu Kitazume <u.kitazume@gmail.com>
2015-01-12 11:52:19 +0900

    fix appname

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,5 +2,5 @@ on_app_master do
   require 'json'
   keys = JSON.parse(File.read("#{config.shared_path}/config/ey_services_config_deploy.json"))
   license_key = keys['New Relic']['license_key']
-  run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=#{config.app} --environment=#{config.environment_name} --user=\"#{config.deployed_by}\" --revision=#{config.active_revision} --license-key=#{license_key}"
+  run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=\"todo / todo_prd\" --environment=#{config.environment_name} --user=\"#{config.deployed_by}\" --revision=#{config.active_revision} --license-key=#{license_key}"
 end
