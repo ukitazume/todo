7c709a3243f622ca1191a851e9e367f46abea8ca
Yu Kitazume <u.kitazume@gmail.com>
2015-01-12 10:31:46 +0900

    deploy notices for newrelic

---
+++ deploy/after_restart.rb
@@ -0,0 +1,1 @@
+run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=#{config.app} --environment=#{config.environment_name} --user=#{config.deployed_by} --revision=#{config.active_revision}"
