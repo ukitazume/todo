a321ba2ad11be66eac0b04924ac764395804f8ee
Yu Kitazume <u.kitazume@gmail.com>
2015-01-12 11:41:32 +0900

    add key

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,6 +1,6 @@
 on_app_master do
   require 'json'
-  keys = JSON.parse(File.read("#{config/shared_path}/config/ey_services_config_deploy.json"))
+  keys = JSON.parse(File.read("#{config.shared_path}/config/ey_services_config_deploy.json"))
   license_key = keys['New Relic']['license_key']
   run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=#{config.app} --environment=#{config.environment_name} --user=#{config.deployed_by} --revision=#{config.active_revision} --license_key=#{license_key})}"
 end
