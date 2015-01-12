b79ffe74052dd059354ea64a6863b11eeaf0a415
Yu Kitazume <u.kitazume@gmail.com>
2015-01-12 12:01:32 +0900

    fix

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,6 +2,6 @@ on_app_master do
   require 'json'
   keys = JSON.parse(File.read("#{config.shared_path}/config/ey_services_config_deploy.json"))
   license_key = keys['New Relic']['license_key']
-  app_name =  json['base']['app_environment_name']
+  app_name =  keys['base']['app_environment_name']
   run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=\"#{app_name}\" --environment=#{config.environment_name} --user=\"#{config.deployed_by}\" --revision=#{config.active_revision} --license-key=#{license_key}"
 end
