1b72f7c36527b1c4f49a2546ee68d6bd886f1bc9
Yu Kitazume <u.kitazume@gmail.com>
2015-01-12 10:52:47 +0900

    add key

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,1 +1,4 @@
-run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=#{config.app} --environment=#{config.environment_name} --user=#{config.deployed_by} --revision=#{config.active_revision}"
+on_app_master do
+  require "ey_config"
+  run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=#{config.app} --environment=#{config.environment_name} --user=#{config.deployed_by} --revision=#{config.active_revision} --license_key=#{EY::Config.get("New Relic", 'license_key')}"
+end
