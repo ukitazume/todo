64fe7f39d55dc81280f2e568f8af2bfc6202d7e4
Yu Kitazume <u.kitazume@gmail.com>
2015-01-22 17:00:25 +0900

    fix var

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -7,13 +7,10 @@ on_utilities("sidekiq") do
   sudo "monit unmonitor all -g #{config.app}_sidekiq"
 
   (0...worker_count).each do |i|
-    pid_file = "/var/run/engineyard/sidekiq/#{config.app}/sidekiq_#{1}.pid"
-    info "TERM sidekiq #{i}"
+    pid_file = "/var/run/engineyard/sidekiq/#{config.app}/sidekiq_#{i}.pid"
     run "kill -TERM `cat #{pid_file}`" if File.exist?(pid_file)
   end
 
-  info "waiting for sidekiq timeout.. "
   sleep sidekiq_timeout
-  info "monitor start"
   sudo "monit monitor all -g #{config.app}_sidekiq"
 end
--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -2,8 +2,7 @@ on_utilities("sidekiq") do
   worker_count = 1
 
   (0...worker_count).each do |i|
-    pid_file = "/var/run/engineyard/sidekiq/#{config.app}/sidekiq_#{1}.pid"
-    info "stop reciveing any job -  sidekiq #{i}"
+    pid_file = "/var/run/engineyard/sidekiq/#{config.app}/sidekiq_#{i}.pid"
     run "kill -USR1 `cat #{pid_file}`" if File.exist?(pid_file)
   end
 end
