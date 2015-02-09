256de0b085fdda74a9ec2cda79d257e611db21a3
Yu Kitazume <u.kitazume@gmail.com>
2015-02-09 11:32:34 +0900

    remove hook

--- deploy/after_restart.rb
+++
@@ -1,7 +0,0 @@
-on_app_master do
-  require 'json'
-  keys = JSON.parse(File.read("#{config.shared_path}/config/ey_services_config_deploy.json"))
-  license_key = keys['New Relic']['license_key']
-  app_name =  keys['base']['app_environment_name']
-  run "#{config.current_path}/ey_bundler_binstubs/newrelic deployment --appname=\"#{app_name}\" --environment=#{config.framework_env} --user=\"#{config.deployed_by}\" --revision=#{config.active_revision} --license-key=#{license_key}"
-end
