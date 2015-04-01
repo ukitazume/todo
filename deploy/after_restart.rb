306527a187e5ac55632ce7f58e7ac77d9efa1a0e
Yu Kitazume <u.kitazume@gmail.com>
2015-04-01 16:05:46 +0900

    test for dogapi

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
-sudo "gem install dogapi"
+ENV['GEM_PATH']="#{ENV['GEM_HOME']}:/data/todo/shared/bundled_gems/ruby/1.9.1/gems"
 require 'rubygems'
 require 'dogapi'
 dog = Dogapi::Client.new('api_key', 'application_key')
