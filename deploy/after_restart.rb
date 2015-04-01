e81d3512472743818998b2c88dbc738104c7f854
Yu Kitazume <u.kitazume@gmail.com>
2015-04-01 16:12:56 +0900

    datadog

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
-ENV['GEM_PATH']="#{ENV['GEM_HOME']}:/data/todo/shared/bundled_gems/ruby/1.9.1/gems"
+# ENV['GEM_PATH']="#{ENV['GEM_HOME']}:#{config.shared_path}/bundled_gems/ruby/1.9.1/gems"
 require 'rubygems'
 require 'dogapi'
 dog = Dogapi::Client.new('api_key', 'application_key')
