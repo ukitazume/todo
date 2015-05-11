4b40de81cd9331d16077a205215d61ef14446b66
Yu Kitazume <u.kitazume@gmail.com>
2015-05-11 13:34:51 +0900

    remove datadog test code

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,5 +1,1 @@
 # ENV['GEM_PATH']="#{ENV['GEM_HOME']}:#{config.shared_path}/bundled_gems/ruby/1.9.1/gems"
-require 'rubygems'
-require 'dogapi'
-dog = Dogapi::Client.new('4db33cdab1957ca8a48a50a7c364b5de', 'bc327ab47c86ecb17c0c7178350abec3c3a8d0b4')
-dog.emit_event(Dogapi::Event.new('msg_text', :msg_title => 'Title'))
