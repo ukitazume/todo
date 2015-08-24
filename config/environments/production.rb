3bd7fd510ccdbd18738f8fc88b2284b00b956a0d
Yu Kitazume <u.kitazume@gmail.com>
2015-08-24 15:07:36 +0900

    add assets prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Listr::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( test/**/*.js )
+  config.assets.prefix = "/next"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
