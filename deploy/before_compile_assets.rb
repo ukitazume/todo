4e2e96df25fa25d1ec3ac15721369e77e36b4368
Yu Kitazume <u.kitazume@gmail.com>
2016-03-14 13:53:37 +0900

    add test commands

--- deploy/before_compile_assets.rb
+++ deploy/before_compile_assets.rb
@@ -1,1 +1,3 @@
 #run! "ln -s #{config.shared_path}/config/aws.yml #{config.release_path}/config/aws.yml"
+run "mkdir -p #{config.release_path}/public/assets"
+run "touch #{config.release_path}/public/assets/#{Time.now}"
